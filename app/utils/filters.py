fead0b90e882a934e7c271b773082c3053201b87
ChaseOstien <129995308+ChaseOstien@users.noreply.github.com>
2023-12-29 16:39:30 -0600

    Updated templates to include filters for dates, plurality and urls

--- app/utils/filters.py
+++ app/utils/filters.py
@@ -1,12 +1,12 @@
-def format_date(date):
+def format_date(date): # formats dates
     return date.strftime('%m/%d/%y')
 
 from datetime import datetime
 
-def format_url(url):
+def format_url(url): # filters urls down to domain name only
     return url.replace('http://','').replace('https://','').replace('www.','').split('/')[0].split('?')[0]
 
-def format_plural(amount, word):
+def format_plural(amount, word): # changes words to plural based on amount
     if amount != 1:
         return word + 's'
     
