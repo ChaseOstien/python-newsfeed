32d9dd06be9d1ecaef4b9e66552b078cb8d1296c
ChaseOstien <129995308+ChaseOstien@users.noreply.github.com>
2023-12-29 16:23:51 -0600

    Established database connection and termination for individual requests, updated home and single post routes to render templates with data from db

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from dotenv import load_dotenv
+from flask import g
 
 load_dotenv()
 
@@ -12,3 +13,24 @@ engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
 print("After database connection code")
+
+# This function initializes the database connection and also closes the database connection
+def init_db(app):
+    Base.metadata.create_all(engine)
+
+    app.teardown_appcontext(close_db)
+
+# This function gets the database connection
+def get_db():
+    if 'db' not in g:
+        # store db connection in app context
+        g.db = Session()
+
+    return Session()
+
+# This function closes the database connection
+def close_db(e=None):
+    db = g.pop('db', None)
+
+    if db is not None:
+        db.close()
